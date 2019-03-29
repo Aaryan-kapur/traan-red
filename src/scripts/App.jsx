3802a61c6617e63250f351ac123d81b45a74a38b
unknown <aniket_chowdhury@hotmail.com>
2019-03-30 04:18:09 +0530

    main-div-graphic-add

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,30 +1,33 @@
 import React, { Component } from "react";
-
+import graphic from "../assests/graphic.svg"
 class Upper extends Component {
   render() {
     return (
-      <div class="main-div">
-        <div class="container">
-          <h1 class="main-div-text">Traan</h1>
-          <div class="row">
-            <div class="main-div-additional responsive-font-size col-sm-9 col-m-9 col-lg-6">
-              <h2 class="text-break">
+      <div className="main-div">
+        <div className="container">
+          <h1 className="main-div-text">Traan</h1>
+          <div className="row">
+            <div className="main-div-additional responsive-font-size col-sm-12 col-m-6 col-lg-6">
+              <h2 className="text-break">
                 {" "}
                 Healing in a matter of time, but it is sometimes also a matter
                 of opportunity.
               </h2>
               <button
                 type="button"
-                class="main-div-button-set btn btn-dark btn-outline-white waves-effect"
+                className="main-div-button-set btn btn-dark btn-outline-white waves-effect"
               >
                 Contact-Us
               </button>
               <button
                 type="button"
-                class="main-div-button-set btn  waves-effect"
+                className="main-div-button-set btn  waves-effect"
               >
                 Know More{" "}
               </button>
+            </div>
+            <div className="main-div-graphic responsive-font-size col-sm-12 col-m-6 col-lg-6">
+            <img src={graphic} alt=""/>
             </div>
           </div>
         </div>
