849e9d4f0c288e86d747b4d38df1df29f0d27851
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-03-30 04:04:36 +0530

    Upper component

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,35 +1,43 @@
 import React, { Component } from "react";
 
+class Upper extends Component {
+  render() {
+    return (
+      <div class="main-div">
+        <div class="container">
+          <h1 class="main-div-text">Traan</h1>
+          <div class="row">
+            <div class="main-div-additional responsive-font-size col-sm-9 col-m-9 col-lg-6">
+              <h2 class="text-break">
+                {" "}
+                Healing in a matter of time, but it is sometimes also a matter
+                of opportunity.
+              </h2>
+              <button
+                type="button"
+                class="main-div-button-set btn btn-dark btn-outline-white waves-effect"
+              >
+                Contact-Us
+              </button>
+              <button
+                type="button"
+                class="main-div-button-set btn  waves-effect"
+              >
+                Know More{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <div class="main-div">
-          <div class="container">
-            <h1 class="main-div-text">Traan</h1>
-            <div class="row">
-              <div class="main-div-additional responsive-font-size col-sm-9 col-m-9 col-lg-6">
-                <h2 class="text-break">
-                  {" "}
-                  Healing in a matter of time, but it is sometimes also a matter
-                  of opportunity.
-                </h2>
-                <button
-                  type="button"
-                  class="main-div-button-set btn btn-dark btn-outline-white waves-effect"
-                >
-                  Contact-Us
-                </button>
-                <button
-                  type="button"
-                  class="main-div-button-set btn  waves-effect"
-                >
-                  Know More{" "}
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Upper/>
       </React.Fragment>
     );
   }
