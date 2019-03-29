a67abdc2f6134ff191b55c56307c24d58ea9f00e
unknown <aniket_chowdhury@hotmail.com>
2019-03-30 05:28:30 +0530

    init-cards-div

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -3,7 +3,98 @@ import graphic from "../assests/graphic.svg"
 class Cards extends Component{
   render(){
     return(
-      <div className="test">Hola</div>
+      <div className="cards-clients">
+      <div className="container">
+      <div className="row">
+      <div className="card1 col-sm-12 col-m-4 col-lg-4">
+      <div className="card card-cascade wider">
+
+  <div className="view view-cascade overlay">
+    <img  className="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/photo6.jpg" alt="Card image cap" />
+    <a href="#!">
+      <div className="mask rgba-white-slight"></div>
+    </a>
+  </div>
+
+ 
+  <div className="card-body card-body-cascade text-center">
+
+    <h4 className="card-title"><strong>Alison Belmont</strong></h4>
+   
+    <h5 className="blue-text pb-2"><strong>Graffiti Artist</strong></h5>
+    
+    <p className="card-text">Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque laudantium, totam rem aperiam. </p>
+
+  </div>
+    <a className="px-2 fa-lg li-ic"><i className="fab fa-linkedin-in"></i></a>
+    <a className="px-2 fa-lg tw-ic"><i className="fab fa-twitter"></i></a>
+    <a className="px-2 fa-lg fb-ic"><i className="fab fa-facebook-f"></i></a>
+
+  </div>
+
+</div>
+<div className="card1 col-sm-12 col-m-4 col-lg-4">
+      <div className="card card-cascade wider">
+
+  <div className="view view-cascade overlay">
+    <img  className="card-img-top" src={graphic} alt="Card image cap" />
+    <a href="#!">
+      <div className="mask rgba-white-slight"></div>
+    </a>
+  </div>
+
+ 
+  <div className="card-body card-body-cascade text-center">
+
+    <h4 className="card-title"><strong>Alison Belmont</strong></h4>
+   
+    <h5 className="blue-text pb-2"><strong>Graffiti Artist</strong></h5>
+    
+    <p className="card-text">Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque laudantium, totam rem aperiam. </p>
+
+  </div>
+    <a className="px-2 fa-lg li-ic"><i className="fab fa-linkedin-in"></i></a>
+    <a className="px-2 fa-lg tw-ic"><i className="fab fa-twitter"></i></a>
+    <a className="px-2 fa-lg fb-ic"><i className="fab fa-facebook-f"></i></a>
+
+  </div>
+
+</div>
+
+<div className="card1 col-sm-12 col-m-4 col-lg-4">
+      <div className="card card-cascade wider">
+
+  <div className="view view-cascade overlay">
+    <img  className="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/photo6.jpg" alt="Card image cap" />
+    <a href="#!">
+      <div className="mask rgba-white-slight"></div>
+    </a>
+  </div>
+
+ 
+  <div className="card-body card-body-cascade text-center">
+
+    <h4 className="card-title"><strong>Alison Belmont</strong></h4>
+   
+    <h5 className="blue-text pb-2"><strong>Graffiti Artist</strong></h5>
+    
+    <p className="card-text">Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque laudantium, totam rem aperiam. </p>
+
+  </div>
+    <a className="px-2 fa-lg li-ic"><i className="fab fa-linkedin-in"></i></a>
+    <a className="px-2 fa-lg tw-ic"><i className="fab fa-twitter"></i></a>
+    <a className="px-2 fa-lg fb-ic"><i className="fab fa-facebook-f"></i></a>
+
+  </div>
+
+</div>
+
+
+
+      </div>
+      </div>
+      </div>
+     
     )
   }
 }
