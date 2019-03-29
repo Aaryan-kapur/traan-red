718d6de3ac0264a08b500b8e57330bea3d7a9c2d
unknown <aniket_chowdhury@hotmail.com>
2019-03-30 03:19:12 +0530

    init-main-div

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.scss';
 import App from './scripts/App';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import './styles/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
