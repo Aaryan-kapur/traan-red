437d00ed900a8cbcf30ff2c9de96c9cb6448cf31
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-04-05 04:51:27 +0530

    Updates

---
+++ precache-manifest.567cd9499777b8d6d1a0a611981760a6.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "1b432123b3f6b7e573ae",
+    "url": "aaryan-kapur.github.io/traan-red/static/css/main.7a536078.chunk.css"
+  },
+  {
+    "revision": "c7f3ac426ef1e092e689",
+    "url": "aaryan-kapur.github.io/traan-red/static/js/runtime~main.e5286006.js"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "4d6263247ae47ace3523",
+    "url": "aaryan-kapur.github.io/traan-red/static/js/2.c7ea953c.chunk.js"
+  },
+  {
+    "revision": "1089f45b26b045dd9803a8716304a714",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/red1.1089f45b.svg"
+  },
+  {
+    "revision": "46cc4db547a57ee8f32ff549890c41c4",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/graphic.46cc4db5.svg"
+  },
+  {
+    "revision": "32b5e312160c7e13b8f2f9b84daf3406",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/logo.32b5e312.png"
+  },
+  {
+    "revision": "7d4fc4b695ef1faa6cbf3c9842afa0a6",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Card1.7d4fc4b6.svg"
+  },
+  {
+    "revision": "439e4ec28549312b4ca57991e0e6b397",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Card2.439e4ec2.svg"
+  },
+  {
+    "revision": "d7dbcd21d4089b774bff95d70b6c28be",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Card3.d7dbcd21.svg"
+  },
+  {
+    "revision": "de09909c24130365541469a954890539",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/red2.de09909c.svg"
+  },
+  {
+    "revision": "79f326a8be2a1191fb7a6e94fe48b03c",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/red3.79f326a8.svg"
+  },
+  {
+    "revision": "daf948a59281578c827836dd44bedf3c",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/map.daf948a5.png"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "1b432123b3f6b7e573ae",
+    "url": "aaryan-kapur.github.io/traan-red/static/js/main.8b94c8ac.chunk.js"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "aaryan-kapur.github.io/traan-red/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "4d6263247ae47ace3523",
+    "url": "aaryan-kapur.github.io/traan-red/static/css/2.cc65e9fc.chunk.css"
+  },
+  {
+    "revision": "f1c86e3b64339a08d7b7b478875d6bd9",
+    "url": "aaryan-kapur.github.io/traan-red/index.html"
+  }
+];
